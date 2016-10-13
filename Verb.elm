23e245e87b6e93ff3d81db4217dd224203f0a21d
Szymon Pyżalski <szymon@pythonista.net>
2016-10-13 21:10:06 +0200

    [WIP] It compiles!

--- Verb.elm
+++ Verb.elm
@@ -1,3 +1,4 @@
+module Verb where
 import List 
 
 type Declension = FirstDeclension
@@ -6,16 +7,31 @@ type Person = FirstPerson | SecondPerson | ThirdPerson
 
 type Screeve = Present
 
-type Series = PresentSeries | FutureSeries | AoristSeries | PerfectSeries
+type Series = PresentSeries --| FutureSeries | AoristSeries | PerfectSeries
 
 series : Screeve -> Series
 
 series Present = PresentSeries
 
 type alias Verb = {
-    preverb: string,
-    root: string,
-    suffix: string
+    preverb: String,
+    version: String,
+    root: String,
+    suffix: String,
+    declension: Declension
+}
+
+preverb screeve verb  = verb.preverb
+
+suffix screeve verb = verb.suffix
+
+root screeve verb = verb.root
+
+version screeve verb = verb.version
+
+type alias PersonNumber = {
+    person: Person,
+    plural: Bool
 }
 
 vMarkers personnumber =
@@ -23,47 +39,41 @@ vMarkers personnumber =
         prefix = case personnumber.person of
             FirstPerson -> "ვ"
             _ -> ""
-        postfix = if personnumber.plural && personnumber.person != ThirdPerson then "" else "თ" 
+        postfix = if personnumber.plural && personnumber.person /= ThirdPerson then "" else "თ" 
     in
-        (prefix, postfix)
+        (prefix, "", postfix)
 
 screeveMarker screeve subject = 
-    let def, sing3, pl3 = case screeve of
+    let (def, sing3, pl3) = case screeve of
         Present -> ("", "ს", "ენ")
     in 
-        case subject.person, subject.plural of
-            (ThirdPerson, false) -> sing3
-            (ThirdPerson, true) -> pl3
+        case (subject.person, subject.plural) of
+            (ThirdPerson, False) -> sing3
+            (ThirdPerson, True) -> pl3
             _ -> def
         
 
-normalPersonMarkers : Screeve -> PersonNumber -> Verb
-normalPersonMarkers screeve subject verb =
-    if member 
-        |> series screeve
-        |> [PresentSeries]
-    then
-        vMarkers subject
 
-personMarkers : Screeve -> PersonNumber -> Verb
-personMarkers screeve subject verb = 
-    if member
-        |> declension verb 
-        [FirstDeclension]
-    then
-        normalPersonMarkers screeve subject verb
+normalPersonMarkers : Screeve -> PersonNumber -> Verb -> (String, String, String)
+normalPersonMarkers screeve subject verb = case series screeve of
+    PresentSeries -> vMarkers subject
 
 
-decline : Screeve -> PersonNumber -> Verb
+personMarkers : Screeve -> PersonNumber -> Verb -> (String, String, String)
+personMarkers screeve subject verb = case verb.declension of
+        FirstDeclension -> normalPersonMarkers screeve subject verb
+
+
+decline : Screeve -> PersonNumber -> Verb -> String
 decline screeve subject verb =
     let
         (pfxp, sfxp, plural) = personMarkers screeve subject verb
     in
-        (preverb screeve verb) +
-        pfxp +
-        version verb +
-        root screeve verb +
-        suffix screeve verb +
-        screeveMarker screeve subject +
-        sfxp +
+        (preverb screeve verb) ++
+        pfxp ++
+        version screeve verb ++
+        root screeve verb ++
+        suffix screeve verb ++
+        screeveMarker screeve subject ++
+        sfxp ++
         plural
